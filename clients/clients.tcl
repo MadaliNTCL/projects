0aceed58428f7d77754f489b019bc318574b68dd
MadaliN <madalinmen28@gmail.com>
2018-03-07 12:46:39 +0200

    Create clients.tcl

---
+++ clients/clients.tcl
@@ -0,0 +1,8 @@
+Commands:
+
+<_MaDaLiN_>!client add [name] [dd-mm-yy]
+<BOTNICK>Client 'Name' (id: 34321) added succesfully. For more informations about data's to add use !client info
+
+<_MaDaLiN_>!client info
+<BOTNICK>Available commands:
+<BOTNICK>!client 
