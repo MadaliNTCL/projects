112c7499a926101745bfd678e075e591818f02d3
MadaliN <madalinmen28@gmail.com>
2018-03-08 15:22:26 +0200

    Update clients.tcl

--- clients/clients.tcl
+++ clients/clients.tcl
@@ -1,8 +1,19 @@
 Commands:
 
-<_MaDaLiN_>!client add [name] [dd-mm-yy]
-<BOTNICK>Client 'Name' (id: 34321) added succesfully. For more informations about data's to add use !client info
+USAGE:
+!client <add> [name] <server/conx/znc/eggdrop/psybnc/ppr> [dd-mm-yy]
 
-<_MaDaLiN_>!client info
-<BOTNICK>Available commands:
-<BOTNICK>!client 
+<_MaDaLiN_>!client add [name] [dd-mm-yy]
+<BOTNICK>Client 'Name' added succesfully. Available variables to add 'server/conx/znc/eggdrop/psybnc/price per conx/network'
+
+<_MaDaLiN_>!client [name]
+<BOTNICK>Account name: Madalin (id: 34321). Will expire in 3 days, 2 hours, 3 minutes.
+
+<_MaDaLiN_>!client info Madalin server
+<BOTNICK>Account name: 'Madalin' - Server: UnderNET
+
+<_MaDaLiN_>!client stats
+<BOTNICK>Found 3 users in the database.
+
+<_MaDaLiN_>!client add Madalin server UnderNET
+<_MaDaLiN_>!client add Madalin conx 3
