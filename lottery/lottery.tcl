c4ac2448508abc6d0f7d3e88edc0e0b8cd4390c0
MadaliN <madalinmen28@gmail.com>
2018-03-25 20:37:11 +0300

    Update lottery.tcl

--- lottery/lottery.tcl
+++ lottery/lottery.tcl
@@ -1,1 +1,14 @@
-as
+/msg Lottery 4 7 8
+<BOTNICK> Congrats, your numbers are: 4, 7, 8
+
+/msg Lottery numbers
+<BOTNICK> Your lucky numbers are: 4, 7, 8
+
+/msg Lottery lastWon
+<BOTNICK> You last guess number/s '7'. That was 6 weeks 4 days 2 hours 2 minutes.
+
+/msg Lottery draw
+<BOTNICK> The draw is in 2 days 2 hours 3 minutes.
+
+/msg Lottery lastNumbers
+<BOTNICK> Last '2' drawn numbers where: 7, 8
