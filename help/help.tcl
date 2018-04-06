ee33f3866b393e6e14df15573c1c5ee9d1b10a17
MadaliN <madalinmen28@gmail.com>
2018-04-06 19:48:45 +0300

    Update help.tcl

--- help/help.tcl
+++ help/help.tcl
@@ -6,5 +6,27 @@ tracker account please use the following command /msg BOTNICK report
 <Madalin>report
 <BOTNICK>Welcome to our IRC Ticket Support
 <BOTNICK>Use the following commands (ignore '<', '>', '[', ']')
-<BOTNICK>-- report -bug [message], if you found a bug and you want to report it.
-<BOTNICK>-- report -disable [message], ....
+<BOTNICK>report -bug [your message]
+<BOTNICK>report -disable [your message]
+<BOTNICK>report -help [your message]
+
+<Madalin>report -bug Page morethantv.org/staff.php gives 'ERROR 404'
+<BOTNICK>Your BUG REPORT [id: 2312] has been succesfully sent. As soon as we will solve this you will receive a message (if you are online at that time)
+
+<Madalin>report list
+<BOTNICK>I currently have 1 bug report, 0 disable, 3 help.
+<BOTNICK>To read one first use /msg BOTNICK report list -bug/-disable/-help and then report -read [id]
+
+<Madalin>report -read 2312
+<BOTNICK>Ticket info:
+<BOTNICK>Created by Madalin (*!*@TCLSCripting.users.undernet.org) - 3 hours 2 minutes 12 seconds ago
+<BOTNICK>Category: bug
+<BOTNICK>Text: Page morethantv.org/staff.php gives 'ERROR 404'
+<BOTNICK>Status: IN PROGRESS
+
+<Madalin>report -solve 2312 Page working now, please check and answer. Thanks
+<BOTNICK>Ticket with id 2312 answered.
+
+<Madalin>report -info 2312
+<BOTNICK>Ticket info:
+<BOTNICK>
