2c7d159620340c163fcb2c0db5bcd9b59dad710f
MadaliN <madalinmen28@gmail.com>
2018-04-06 19:52:06 +0300

    Update help.tcl

--- help/help.tcl
+++ help/help.tcl
@@ -3,6 +3,15 @@ Madalin joins #help
 <BOTNICK> Welcome Madalin to the official #asgasg help channel. Currently there is no ADMIN online. If you have any problem regarding your
 tracker account please use the following command /msg BOTNICK report
 
+====== Admin commands
+<Madalin>report list
+<BOTNICK>I currently have 1 bug report, 0 disable, 3 help.
+<BOTNICK>To read one first use /msg BOTNICK report list -bug/-disable/-help and then report -read [id]
+
+<Madalin>report -solve 2312 Page working now, please check and answer. Thanks
+<BOTNICK>Ticket with id 2312 answered.
+
+====== User commands
 <Madalin>report
 <BOTNICK>Welcome to our IRC Ticket Support
 <BOTNICK>Use the following commands (ignore '<', '>', '[', ']')
@@ -13,10 +22,6 @@ tracker account please use the following command /msg BOTNICK report
 <Madalin>report -bug Page morethantv.org/staff.php gives 'ERROR 404'
 <BOTNICK>Your BUG REPORT [id: 2312] has been succesfully sent. As soon as we will solve this you will receive a message (if you are online at that time)
 
-<Madalin>report list
-<BOTNICK>I currently have 1 bug report, 0 disable, 3 help.
-<BOTNICK>To read one first use /msg BOTNICK report list -bug/-disable/-help and then report -read [id]
-
 <Madalin>report -read 2312
 <BOTNICK>Ticket info:
 <BOTNICK>Created by Madalin (*!*@TCLSCripting.users.undernet.org) - 3 hours 2 minutes 12 seconds ago
@@ -24,9 +29,13 @@ tracker account please use the following command /msg BOTNICK report
 <BOTNICK>Text: Page morethantv.org/staff.php gives 'ERROR 404'
 <BOTNICK>Status: IN PROGRESS
 
-<Madalin>report -solve 2312 Page working now, please check and answer. Thanks
-<BOTNICK>Ticket with id 2312 answered.
-
 <Madalin>report -info 2312
 <BOTNICK>Ticket info:
-<BOTNICK>
+<BOTNICK>This ticket has been solved by Vasile - 1 hour 3 minutes ago. 
+<BOTNICK>Question: Page morethantv.org/staff.php gives 'ERROR 404'
+<BOTNICK>Answer: Page working now, please check and answer. Thanks
+<BOTNICK>Ticket will auto delete in 1 day.
+
+== You cannot read a ticket (that is not made by you even if you know its ID)
+== You can delete your own ticket
+== Admins can delete/solve ticket
