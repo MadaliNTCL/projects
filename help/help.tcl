59b84aa28c31e2985bc7fffc905f4421481e44b8
MadaliN <madalinmen28@gmail.com>
2018-04-06 13:27:51 +0300

    Update help.tcl

--- help/help.tcl
+++ help/help.tcl
@@ -1,1 +1,10 @@
 
+Madalin joins #help
+<BOTNICK> Welcome Madalin to the official #asgasg help channel. Currently there is no ADMIN online. If you have any problem regarding your
+tracker account please use the following command /msg BOTNICK report
+
+<Madalin>report
+<BOTNICK>Welcome to our IRC Ticket Support
+<BOTNICK>Use the following commands (ignore '<', '>', '[', ']')
+<BOTNICK>-- report -bug [message], if you found a bug and you want to report it.
+<BOTNICK>-- report -disable [message], ....
