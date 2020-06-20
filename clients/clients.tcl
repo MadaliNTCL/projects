cb378e342ef0d557220d05402cb048e1950b15de
MadaliN <madalinmen28@gmail.com>
2020-06-20 16:03:41 +0300

    Update clients.tcl

--- clients/clients.tcl
+++ clients/clients.tcl
@@ -1,19 +1,16 @@
-Commands:
+/msg BOTNICK addclient Vasile *!*@Vasile.users.undernet.org
+/msg BOTNICK editclient Vasille expiration zz/ll/an
+/msg BOTNICK editclient Vasile trivia <numar> <pret>
+/msg BOTNICK editclient Vasile radio <numar> <pret>
+/msg BOTNICK editclient Vasile eggdrop <numar> <pret>
+/msg BOTNICK editclient Vasile znc <numar> <pret>
+/msg BOTNICK editclient Vasile emech <numar> <pret>
 
-USAGE:
-!client <add> [name] <server/conx/znc/eggdrop/psybnc/ppr> [dd-mm-yy]
 
-<_MaDaLiN_>!client add [name] [dd-mm-yy]
-<BOTNICK>Client 'Name' added succesfully. Available variables to add 'server/conx/znc/eggdrop/psybnc/price per conx/network'
+Account name: Vasile (*!*@Vasile.users.undernet.org)
+Creation date: 20/06/2020 (12 ani 4 luni ...)
+Expiration date: 20/06/2020 (19 zile 10 ore 23 minute...)
+Trivia: 2 (Ascenture + Andromeda) =  6 euro
+Radio: 1 (Ascenture) = 1 euro
+Total: 7 euro
 
-<_MaDaLiN_>!client [name]
-<BOTNICK>Account name: Madalin (id: 34321). Will expire in 3 days, 2 hours, 3 minutes.
-
-<_MaDaLiN_>!client info Madalin server
-<BOTNICK>Account name: 'Madalin' - Server: UnderNET
-
-<_MaDaLiN_>!client stats
-<BOTNICK>Found 3 users in the database.
-
-<_MaDaLiN_>!client add Madalin server UnderNET
-<_MaDaLiN_>!client add Madalin conx 3
